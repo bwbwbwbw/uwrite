5b610f8592206fd187ec30150523d0a3d81d7868
Breezewish <me@breeswish.org>
2015-05-25 20:39:25 +0800

    initial commit

---
+++ src/main/java/edu/tongji/signin/SigninController.java
@@ -0,0 +1,13 @@
+package edu.tongji.signin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class SigninController {
+
+	@RequestMapping(value = "signin")
+	public String signin() {
+        return "signin/signin";
+    }
+}
