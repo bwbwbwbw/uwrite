589d60402aac253f3d319c517bd8b5847be5b12f
Breezewish <me@breeswish.org>
2015-06-16 11:16:13 +0800

    move original index to topic

--- src/main/webapp/resources/js/topics.js
+++ src/main/webapp/resources/js/topics.js
@@ -1,24 +1,14 @@
-/**
- * hovers.js v1.0.0
- * http://www.codrops.com
- *
- * Licensed under the MIT license.
- * http://www.opensource.org/licenses/mit-license.php
- * 
- * Copyright 2014, Codrops
- * http://www.codrops.com
- */
 (function() {
 	
 	function init() {
-		var speed = 250,
-			easing = mina.easeinout;
+		var speed = 330,
+			easing = mina.backout;
 
 		[].slice.call ( document.querySelectorAll( '#grid > a' ) ).forEach( function( el ) {
 			var s = Snap( el.querySelector( 'svg' ) ), path = s.select( 'path' ),
 				pathConfig = {
 					from : path.attr( 'd' ),
-					to : el.getAttribute( 'data-path-to' )
+					to : el.getAttribute( 'data-path-hover' )
 				};
 
 			el.addEventListener( 'mouseenter', function() {
